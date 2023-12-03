ad163a0ea8534b8b70b7af9bf83c927c7ffe54ee
Hammad Scheikh <140610613+Sheikhhammadahmed@users.noreply.github.com>
2023-12-03 23:05:38 +0500

    Add files via upload

---
+++ JavaScript Assignments/JS Assignment  02/app.js
@@ -0,0 +1,162 @@
+// Chap no 5-7 Maths Expressions
+
+
+
+// Q no 01:
+
+// var a = +prompt('Enter Val 01')
+// var b = +prompt('Enter Val 02')
+// var add = a+b
+
+// document.write(add)
+
+
+// Q no 02:
+
+// var a = +prompt('Enter Val 01')
+// var b = +prompt('Enter Val 02')
+// var add = a + b
+// var sub = a - b
+// var mul = a * b
+// var div = a / b
+// var mod = a % b
+
+
+// Q no 03 :
+
+// var celcius = 12
+// var celtofar = (12*9/5)+32
+// var farhenheit = 68
+// var fartocel = (farhenheit-32)*5/9
+
+// document.write(celcius+'°C'+' is '+celtofar+'°F'+'</br>'+farhenheit+"°F"+' is '+fartocel+"°C")
+
+
+// Q no 04:
+
+// var ticket = 600
+// var tickets = 'The cost of 5 tickets is '+ticket*5
+
+// document.write(tickets)
+
+
+// Q no 05:
+
+// var a = prompt('Enter Digit')
+// var b = prompt('Enter Length')
+
+// for (var i = 1; 10 >= i; i++) {
+//     document.write(a + ' x ' + i + ' = ' + a * i +'</br>')
+// }
+
+
+
+// Q no 06:
+
+// var celcius = "12°"
+// var far
+
+
+// Qno 07:
+
+// var a = +prompt('Price of Item 01')
+// var b = +prompt('Enter Quantity 01')
+// var c = +prompt('Price of Item 02')
+// var d = +prompt('Enter Quantity 02')
+// var e = 150
+// let total_amount =  a * b + c * d + e 
+
+// document.write('Price of Item 01 : ' + a+ "</br>" + 'Quantity of Item 01 : ' + b + "</br>" + 'Price of Item 02 : ' + c + '</br>' + 'Quantity of Item 02 : ' + d + '</br>' + 'Servicing Charges :  ' + e  + '</br>'+'Total Amount of Your Order is :' +total_amount+ '  PKR')
+
+
+// Q no 08 :
+
+// var obt_marks = +prompt('Enter Obtained Marks')
+// var total_marks = +prompt('Enter Total Marks')
+// var perc = obt_marks / total_marks * 100
+
+// document.write("Total Marks : " + total_marks + "</br>" + "Obtained Marks : " + obt_marks + "</br>" + "Percentage : " + perc + '%')
+
+
+// Q no 09:
+
+// var $dollar = 280
+// var riyal = 80
+
+// var pak_to_$ = +(10*$dollar)
+// var pak_to_R = +(10*riyal)
+// let total = (pak_to_$+pak_to_R)
+
+// document.write('Total Currency in PKR: '+total)
+
+
+
+// Q no 10:
+
+// var num1 = 11+5*10/2
+// console.log(num1);
+
+
+// Q no 11 :
+
+
+// var current_year = prompt("Enter Current Year")
+// var birth_year = prompt("Enter Birth Year")
+// var age = current_year-birth_year
+
+// document.write("Current Year : "+current_year+'</br>'+'Birth Year : '+birth_year+'</br>'+"Your Age : "+age)
+
+
+// Q no 12:
+
+
+// var radius = prompt("Enter The Radius ")
+// var circum = 2 * 3.142 * radius
+// var area = 3.142*radius*radius;
+
+// document.write("Radius of a Circle = " + radius + '</br>' + 'The circumference is = ' + circum + '</br>' + "The area is = "+area)
+
+
+
+// Q no 13:
+
+
+// var num = +prompt('Enter Number')
+
+// var eq = ++num
+// var eq2 = num++
+// var eq3 = --num
+
+// document.write('The Default Value of num is =' + num + '</br>' + 'The Value of ++num is = ' + eq + '</br>' + 'The Value Of num++ is = ' + eq2 + '</br>' + 'The Value of --num is = '+ eq3 + "</br>")
+
+
+// Q no 15:
+
+// var a = prompt('Enter your Favourite Snacks')
+// var b = prompt('Enter Current Age')
+// var c = prompt("Enter Maximum Age ")
+// var d = prompt('Enter Amount of Per day ')
+
+// document.write("Favourite Snacks : " + a + '</br>' + 'Current Age : ' + b + '</br>' + 'Maximum Age : ' + c + '</br>' + 'No of Snacks Per Day : ' + d + '</br>' + 'You will need ' + c * d + a + ' for the Rest of Your life. ')
+
+
+
+
+
+// Increment / Decrement Assignments:
+
+
+// var num1 = 4
+// var num2 = 2
+// var num3 = 3
+
+// var eq = 10 + 4 + num1++ - num1--  + num3++ + ++num3 - --num2 + 2 + num1++ + num2 + num3
+
+
+// var eq2 = num1++ + ++num1 + num2++ + num2-- + num3-- + num3++ + num3-- + --num2 + num1++ + num3++ + ++num1
+
+// console.log(eq2);
+
+
+
+
---
+++ JavaScript Assignments/JS Assignment 03/app.js
@@ -0,0 +1,18 @@
+// Chap name /
+
+// Q no 01:
+
+// var a = prompt('Enter Your Name')
+// document.write("Hi "+a)
+
+// Q no 02:
+
+// var a = prompt("Enter Number")
+// var b = prompt("Enter Length")
+
+// for (var i = 1; i <= b; i++) {
+//     document.write(a + ' x ' + i + " = " + a * i + '<br>')
+// }
+
+// Q no 03:
+
